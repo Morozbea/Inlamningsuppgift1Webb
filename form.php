7f23066011f01520e2b9604833c62bafabca8281
Morozbea <morozbea@freemail.hu>
2017-04-18 22:10:02 +0200

    Add changes

---
+++ form.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+ <link href="style.css" type="text/css" rel="stylesheet">
+ <link href="https://fonts.googleapis.com/css?family=Raleway" type="text/css" rel="stylesheet" >
+ <form action="index.php" method="get">
+
+</head>
+
+<body>
+
+<h1>Visa alla enhörningar</h1>
+<h2>Enhörningar</h2>
+ 	<p style="border-bottom: 0.1px solid black;">
+		<div class="idEnhörning">
+			<h3>id på enhörningar</h3>
+			<input type="text" idFor="idUnic"><br>
+			<button class="ShowOneU" type="submit" class="msgBtn" onClick="return false;" >Visa enhörning!</button>
+			<button class="ShowAllU" type="submit" class="msgBtn" onClick="return false;" >Visa alla enhörningar!</button><br>
+			<p style="border-bottom: 0.1px solid black;">
+		</div>
+	<p>Alla Enhörningar: </p>
+		<div class="listofenhörning">
+			<ol>
+ 				<li class="left">Nordsvensk travhörning</li>
+  			    <button class="right" type="submit" class="msgBtn" onClick="return false;" >Läs mer...</button><br>
+ 			    <p style="border-bottom: 0.1px solid black;">
+
+				<li class="left">Karibisk strandponny</li>
+				<button class="right1" type="submit" class="msgBtn" onClick="return false;" >Läs mer...</button><br>
+				<p style="border-bottom: 0.1px solid black;">
+
+				<li class="left">Nattaktiv hornlöpare</li>
+				<button class="right2" type="submit" class="msgBtn" onClick="return false;" >Läs mer...</button><br>
+				<p style="border-bottom: 0.1px solid black;">
+
+				<li class="left">Tordönsenhörning</li>
+				<button class="right3" type="submit" class="msgBtn" onClick="return false;" >Läs mer...</button><br>
+				<p style="border-bottom: 0.1px solid black;">
+
+				<li class="left">Indisk Enhörning</li>
+				<button class="right4" type="submit" class="msgBtn" onClick="return false;" >Läs mer...</button><br>
+ 				<p style="border-bottom: 0.1px solid black;">
+
+				<li class="left">Småväxt enhörning</li>
+				<button class="right5" type="submit" class="msgBtn" onClick="return false;" >Läs mer...</button><br>
+				<p style="border-bottom: 0.1px solid black;">
+
+				<li class="left">Transsylvansk kortsvans</li>
+				<button class="right6" type="submit" class="msgBtn" onClick="return false;" >Läs mer...</button><br>
+				<p style="border-bottom: 0.1px solid black;">
+
+				<li class="left">Grekisk pegasusmix</li>
+				<button class="right7" type="submit" class="msgBtn" onClick="return false;" >Läs mer...</button><br>
+				<p style="border-bottom: 0.1px solid black;">
+
+				<li class="left">Eventuellt fejkad enhörning</li>
+				<button class="right8" type="submit" class="msgBtn" onClick="return false;" >Läs mer...</button><br>
+				<p style="border-bottom: 0.1px solid black;">
+
+				<li class="left">Bayersk lättfotad</li>
+				<button class="right9" type="submit" class="msgBtn" onClick="return false;" >Läs mer...</button><br>
+				<p style="border-bottom: 0.1px solid black;">
+
+				<li class="left">Fransk vågman</li>
+				<button class="right10" type="submit" class="msgBtn" onClick="return false;" >Läs mer...</button><br>
+				<p style="border-bottom: 0.1px solid black;">
+
+				<li class="left">Mulfläckad enhörning</li>
+				<button class="right11" type="submit" class="msgBtn" onClick="return false;" >Läs mer...</button><br>
+				<p style="border-bottom: 0.1px solid black;">
+			</ol>
+		</div>
+</body>
+
+</html>
